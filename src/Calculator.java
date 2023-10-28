55a10a71fecf2bc64071c24346dd06e03a7f6da1
thulanimbatha <thulanimbatha06@gmail.com>
2023-10-28 18:16:21 +0200

    added two buttons - delete & clear

--- src/Calculator.java
+++ src/Calculator.java
@@ -22,16 +22,58 @@ public class Calculator implements ActionListener {
     char operator;
 
     public Calculator() {
+        /*instantiate frame*/
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,550);
         frame.setLayout(null);
 
+        /*Instantiate textField*/
         textField = new JTextField();
         textField.setBounds(50,25,300,50);
         textField.setFont(myFont);
+        textField.setEditable(false);   //user cannot input into textField - we only want to output
 
-        frame.add(textField);
+        /*instantiate buttons*/
+        addButton = new JButton("+");
+        subtractButton = new JButton("-");
+        multButton = new JButton("*");
+        divButton = new JButton("/");
+        decimalButton = new JButton(".");
+        equalsButton = new JButton("=");
+        deleteButton = new JButton("Delete");
+        clearButton = new JButton("Clear");
+        /*add them to the buttons array*/
+        functionButtons[0] = addButton;
+        functionButtons[1] = subtractButton;
+        functionButtons[2] = multButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decimalButton;
+        functionButtons[5] = equalsButton;
+        functionButtons[6] = deleteButton;
+        functionButtons[7] = clearButton;
+        /*we want an ActionListener for each button*/
+        for(int i = 0; i<8; i++){
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        /*Now we need to fill the numberButtons array*/
+        for(int i = 0; i<10; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));  //fill array
+            numberButtons[i].addActionListener(this);       //actionListener for each button
+            numberButtons[i].setFont(myFont);               //font
+            numberButtons[i].setFocusable(false);
+        }
+
+        /*These two buttons will look different to the others*/
+        deleteButton.setBounds(50,430,145,50);
+        clearButton.setBounds(205,430,145,50);
+
+        frame.add(deleteButton);    //add delete button
+        frame.add(clearButton);     //add clear button
+        frame.add(textField);   //add textField to frame
         frame.setVisible(true);
     }
 
