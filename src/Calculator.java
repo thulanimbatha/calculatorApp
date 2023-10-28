73eb32b7b31b55d362b13b5ccb91d03502f0bbc0
thulanimbatha <thulanimbatha06@gmail.com>
2023-10-28 12:58:29 +0200

    added attributes, set basics for JFrame in constructor

---
+++ src/Calculator.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];  //Array of buttons for the numbers, size=10
+    JButton[] functionButtons = new JButton[8]; //Array of buttons for the operators
+    /*Now let's list all the function buttons*/
+    JButton addButton,subtractButton,multButton,divButton;
+    JButton decimalButton,equalsButton,deleteButton,clearButton;
+    /*JPanel to hold all the buttons*/
+    JPanel panel;
+
+    Font myFont = new Font("Times New Roman", Font.BOLD,30);
+
+    //Calculator will always work using at least 3 values & 1 operator
+    double num1=0,num2=0,result=0;
+    char operator;
+
+    public Calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,550);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
